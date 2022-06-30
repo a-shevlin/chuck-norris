f3a44ba04f01067594ae1e159799e0d7f4d833c7
Alex Shevlin <alexshevlin1@gmail.com>
2022-06-30 16:08:46 -0700

    create sortedGif() to sort gif search from word in category array.
    Co-authored-by: Zachary Waggoner <CyndaZ42@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -6,6 +6,23 @@ import Norris from './js/chuck-norris.js';
 import NorrisGiphy from './js/giphy-api.js';
 import RandomGiphy from './js/random-giphy';
 
+//Bear, Cow, house, bull, giraffe, ant, cat
+//Steve Jobs, universe, paperboy, 
+//Paris Hilton, Mr. T, the Rock is cooking, King Kong, Obama, King Midas, Charlie Sheen, Saddam Hussein, Steven Seagal, Hellen Keller, President Roosevelt, Bill Gates, Tom Cruise, MacGyver, Ozzy Osbourne, Stevie Wonder, Wilt Chamberlain
+//restaurant, Food Network, gasoline, butter, milkshake, fries
+
+//google, encrypted, PI, errors, code, machine, Street Fighter, database, bugs fix, Towers of Hanoi, World of Warcraft, binary, exceptions, infinite loop, type, MySpace, IE, hard drive
+   //equal, web, hosting, garbage, Oracle
+
+
+//keyboard, alt: Ctrl
+//No one has ever pair-programmed with Chuck Norris and lived to tell about it.
+//Chuck Norris does not need to know about class factory pattern. He can instantiate interfaces.
+//Chuck Norris can spawn threads that complete before they are started
+//Chuck Norris is the ultimate mutex, all threads fear him.
+//If you try to kill -9 Chuck Norris's programs, it backfires.
+//Project managers never ask Chuck Norris for estimations... ever.
+//Chuck Norris is the only human being to display the Heisenberg uncertainty principle - you can never know both exactly where and how quickly he will roundhouse-kick you in the face.
 function getElements(response) {
     $('#norrisQuote').text(response.value);
     let speech = new SpeechSynthesisUtterance();
@@ -20,11 +37,63 @@ function getElements(response) {
       let newLength = (split.length / 2) * 1000;
       audioPlay(newLength);
     }
+    
     //round down to nearest even number
     //take 2ish words per sec
 
     //if length is equal to 10 words wait 5 seconds
     console.log(split);
+    return response.value;
+}
+
+          function sortedGif(response, category) {
+          let arr = [];
+          const str = response.value;
+          if ( category === "food"){
+            arr = ['restaurant', 'Food Network', 'gasoline', 'butter', 'milkshake', 'fries'];
+          }
+          let match = '';
+          const contains = arr.some(element => {
+            if (str.includes(element)) {
+              match = element;
+              return true;
+            }
+            return false;
+          });
+          let categoryNumber = getRandomInt(300);
+          if (contains) {
+            RandomGiphy.getRandom(match, categoryNumber)
+            .then(function(response) {
+              const output = `<img src="${response.data[0].images.downsized_medium.url}"></img>`;
+            })
+            .catch(function(error) {
+              return error;
+            })
+          } else {
+            RandomGiphy.getRandom(category, categoryNumber)
+            .then(function(response) {
+              const output = `<img src="${response.data[0].images.downsized_medium.url}"></img>`;
+            })
+            .catch(function(error) {
+              return error;
+            })
+          }
+          return output;
+          }
+         
+function matchArrayToCategory(category) {
+  let newArray = joke.category;
+  return newArray;
+}
+
+function findMatch(array, value) {
+  let match = array.find(value);
+  if (match === undefined) {
+    match = value;
+  } else {
+    return match;
+  }
+  return match;
 }
 
 function getRandomInt(max) {
@@ -46,7 +115,6 @@ $(document).ready(function() {
     Norris.getRandom(category)
     .then(function(response) {
       getElements(response);
-
     });
     NorrisGiphy.getRandom(norrisNumber)
     .then(function(response) {
